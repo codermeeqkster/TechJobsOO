981d41c57075c8ae0515777548e0fa58d75b3074
Shameeqka Augustin <marketmeeqka@gmail.com>
2019-12-19 18:40:39 -0500

    finish TechJobsOO

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -9,6 +10,10 @@ namespace TechJobs.Controllers
 
         // Our reference to the data store
         private static JobData jobData;
+        private Employer employer;
+        private Location location;
+        private CoreCompetency coreCompetency;
+        private PositionType positionType;
 
         static JobController()
         {
@@ -19,8 +24,8 @@ namespace TechJobs.Controllers
         public IActionResult Index(int id)
         {
             // TODO #1 - get the Job with the given ID and pass it into the view
-
-            return View();
+            Job job = jobData.Find(id);
+            return View(job);
         }
 
         public IActionResult New()
@@ -35,6 +40,54 @@ namespace TechJobs.Controllers
             // TODO #6 - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
+            if (ModelState.IsValid)
+            {
+                foreach (Employer field in jobData.Employers.ToList())
+                {
+                    if(field.ID == newJobViewModel.EmployerID)
+                    {
+                        employer = field;
+                    }
+                }
+
+                foreach (Location field in jobData.Locations.ToList())
+                {
+                    if (field.ID == newJobViewModel.LocationID)
+                    {
+                        location = field;
+                    }
+                }
+
+                foreach (CoreCompetency field in jobData.CoreCompetencies.ToList())
+                {
+                    if (field.ID == newJobViewModel.CoreCompetencyID)
+                    {
+                        coreCompetency = field;
+                    }
+                }
+
+                foreach (PositionType field in jobData.PositionTypes.ToList())
+                {
+                    if (field.ID == newJobViewModel.PositionTypeID)
+                    {
+                        positionType = field;
+                    }
+                }
+
+                Job newJob = new Job
+                {
+                    Name = newJobViewModel.Name,
+                    Employer = employer,
+                    Location = location,
+                    CoreCompetency = coreCompetency,
+                    PositionType = positionType
+                };
+
+                jobData.Jobs.Add(newJob);
+                string newUrl = "/Job?id="+newJob.ID.ToString();
+
+                return Redirect(newUrl);
+            }
 
             return View(newJobViewModel);
         }
--- src/TechJobs/ViewModels/NewJobViewModel.cs
+++ src/TechJobs/ViewModels/NewJobViewModel.cs
@@ -8,15 +8,27 @@ namespace TechJobs.ViewModels
 {
     public class NewJobViewModel
     {
-        [Required]
+        [Required(ErrorMessage ="Please enter a name for the job.")]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please specify an employer.")]
         [Display(Name = "Employer")]
         public int EmployerID { get; set; }
 
         // TODO #3 - Included other fields needed to create a job,
         // with correct validation attributes and display names.
+
+        [Required(ErrorMessage = "Please select a location.")]
+        [Display(Name = "Location")]
+        public int LocationID { get; set; }
+
+        [Required(ErrorMessage = "Please select the desired skill.")]
+        [Display(Name = "Skill")]
+        public int CoreCompetencyID { get; set; }
+
+        [Required(ErrorMessage = "Please select the type of position.")]
+        [Display(Name = "Position Type")]
+        public int PositionTypeID { get; set; }
 
         public List<SelectListItem> Employers { get; set; } = new List<SelectListItem>();
         public List<SelectListItem> Locations { get; set; } = new List<SelectListItem>();
@@ -38,7 +50,23 @@ namespace TechJobs.ViewModels
 
             // TODO #4 - populate the other List<SelectListItem> 
             // collections needed in the view
+            foreach (CoreCompetency field in jobData.CoreCompetencies.ToList())
+            {
+                CoreCompetencies.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
 
+            foreach (PositionType field in jobData.PositionTypes.ToList())
+            {
+                PositionTypes.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
         }
     }
 }
